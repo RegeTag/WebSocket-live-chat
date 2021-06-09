d2ba0bdf6212ec30c06cfa8e407ca49aa6d07ef8
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2021-06-09 13:32:51 -0300

    added nickname support, counter of connected users, nickname change notificaiton and user leave chat notification

--- public/index.js
+++ public/index.js
@@ -1,36 +1,121 @@
 const socket = io("http://localhost:3000")
 
-function addMessage(message){
-    const messageBox = document.getElementById("chatbox")
 
-    const newMessage = document.createElement("div")
-    newMessage.classList.add("message")
+// ================== Functions ======================
+const Messages = {
+    userMessage(message){
+        const nickInput = document.getElementById("nick")
+        const messageBox = document.getElementById("chatbox")
+        const newMessage = document.createElement("div")
+    
+        newMessage.classList.add("message")
+    
+        if(nickInput.value === message.user){
+            newMessage.classList.add("mine")
+        }
+    
+        newMessage.innerHTML = `
+        <strong>${message.user}</strong>
+        <p>${message.message}</p>
+        `
+    
+        messageBox.appendChild(newMessage)
+    },
+    
+    nickChangeMessage(object){
+        const messageBox = document.getElementById("chatbox")
+        const newMessage = document.createElement("div")
 
-    newMessage.innerHTML = `
-    <strong>${message.user}</strong>
-    <p>${message.message}</p>`
+        newMessage.classList.add("message")
+        newMessage.classList.add("system")
+        newMessage.classList.add("nick")
 
-    messageBox.appendChild(newMessage)
+        newMessage.innerHTML = `<strong>"${object.old}"</strong>` + " changed nickname to " + `<strong>"${object.new}"</strong>`
+
+        messageBox.appendChild(newMessage)
+    },
+
+    userLeaveMessage(user){
+        const messageBox = document.getElementById("chatbox")
+        const newMessage = document.createElement("div")
+
+        newMessage.classList.add("message")
+        newMessage.classList.add("system")
+        newMessage.classList.add("leave")
+
+        newMessage.innerHTML = `<strong>"${user}"</strong>` + "left the chat!"
+
+        messageBox.appendChild(newMessage)
+    }
+
 }
 
-document.getElementById("button").addEventListener("click", () => {
+function sendMessageSocket(){
+    const nickInput = document.getElementById("nick")
     const messageInput = document.getElementById("message")
 
-    if(messageInput.value == ""){}
-    else{
-        socket.emit("newMessage", messageInput.value)
+    if(nickInput.value == ""){
+        throw new Error("No nickname set!")
+    }
+
+    if(messageInput.value != ""){
+        socket.emit("newMessage", {"message": messageInput.value, "user": nickInput.value})
     
         messageInput.value = ""
     }
+}
+
+// ================== Event Listeners ================
+
+document.getElementById("button").addEventListener("click", () => {
+    try{
+        sendMessageSocket()
+    }
+    catch(error){
+        alert(error)
+    }
+    
 })
+
+document.getElementById("message").addEventListener("keypress" , e => {
+    if(e.key === "Enter"){
+        try {
+            sendMessageSocket()
+        } catch (error) {
+            alert(error)
+        }
+    }
+})
+
+document.getElementById("nick").addEventListener("change", () => {
+    const nickInput = document.getElementById("nick")
+
+    socket.emit("nickChange", nickInput.value )
+})
+
+//===================== Sockets ======================
 
 socket.on("pastMessages", messages => {
     
     messages.forEach( message => {
-        addMessage(message)
+        Messages.userMessage(message)
     })
 })
 
 socket.on("message", message => {
-    addMessage(message)
+    Messages.userMessage(message)
+})
+
+socket.on("userNumber", number => {
+    const userCounter = document.getElementById("user-counter")
+
+    userCounter.innerHTML = `Users connected: ${number}`
+})
+
+socket.on("nickChange", message => {
+    Messages.nickChangeMessage(message)
+})
+
+socket.on("userDisconnect", user => {
+    Messages.userLeaveMessage(user)
 })
--- src/app.js
+++ src/app.js
@@ -8,23 +8,58 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 var messages = []
+var connectedUsers = []
 
 io.on("connection", socket => {
-    console.log(`Socket:${socket.id} connected`)
-
+    
+    connectedUsers.push({id: socket.id, nick: null})
+    
+    io.emit("userNumber", connectedUsers.length)
+    
     socket.emit("pastMessages", messages)
 
+    socket.on("nickChange", nick => {
+        const User = connectedUsers.find( user => user.id == socket.id)
+
+        const oldNick = User.nick
+        User.nick = nick
+
+        connectedUsers = connectedUsers.map( user => {
+            
+            if(user.id == socket.id){
+                user = User
+            }
+
+            return user
+        })
+
+
+        if(oldNick != null){
+            io.emit("nickChange", {old:oldNick, new:User.nick})
+        }
+    })
+
     socket.on("newMessage", message => {
-        const newMessage = {message: message, user: socket.id}
+        const newMessage = {...message}
 
         messages.push(newMessage)
 
         io.emit("message", newMessage)
     })
+
+    socket.on("disconnect", () => {
+        
+        const user = connectedUsers.find( user => user.id == socket.id)
+        
+        if(user.nick != null){
+            io.emit("userDisconnect", user.nick)
+        }
+
+        connectedUsers = connectedUsers.filter( user => user.id != socket.id)
+        io.emit("userNumber", connectedUsers.length)
+    })
+
 })
-
-
-
 
 app.use(express.static(path.join("public")))
 
